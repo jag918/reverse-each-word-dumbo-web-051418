870a2f26fc271c1967f756a5d03f4c8544e3b276
jee <jag918@nyu.edu>
2018-05-08 02:37:57 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(sentence)
-  words=sentence.split(" ")
-  result = ""
-  words.each do |word|
-    result += " " + word.reverse
-  end
-  result = result.strip
-end
+# def reverse_each_word(sentence)
+#   words=sentence.split(" ")
+#   result = ""
+#   words.each do |word|
+#     result += " " + word.reverse
+#   end
+#   result = result.strip
+# end
 
 def reverse_each_word(sentence)
   words=sentence.split(" ")
